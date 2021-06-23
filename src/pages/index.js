1405862821eceb9a070c7a92f43f81b892dc1539
Stephany <stephany.awino@twiga.com>
2021-06-23 12:52:53 +0300

    contact form

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import * as React from "react";
+import Form from "../components/form";
+import "../global.css";
+
+const ContactForm = () => (
+  <>
+    <h1>Send a message</h1>
+    <Form />
+  </>
+);
+
+export default ContactForm;
